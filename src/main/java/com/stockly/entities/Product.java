7c833d30006613487fafcc0e57578b5b3e363b53
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-08-14 14:31:22 -0300

    Classes de Product

---
+++ src/main/java/com/stockly/entities/Product.java
@@ -0,0 +1,128 @@
+package com.stockly.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.stockly.enums.Status;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String category;
+    private Integer inventory;
+    private BigDecimal price;
+
+    @JsonFormat(pattern = "HH:mm:ss dd-MM-yyyy")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "HH:mm:ss dd-MM-yyyy")
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public Product() {
+    }
+
+    public Product(Long id,
+                   String name,
+                   String category,
+                   Integer inventory,
+                   BigDecimal price,
+                   LocalDateTime createdAt,
+                   LocalDateTime updatedAt,
+                   Status status
+    ) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.inventory = inventory;
+        this.price = price;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Integer inventory) {
+        this.inventory = inventory;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id.equals(product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
