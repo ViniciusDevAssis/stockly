0827703ea58120d4f5af43b201baed417e5c0e05
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-08-14 17:56:55 -0300

    UserController pendente de testes

--- src/main/java/com/stockly/controllers/UserController.java
+++ src/main/java/com/stockly/controllers/UserController.java
@@ -1,7 +1,53 @@
 package com.stockly.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.stockly.entities.User;
+import com.stockly.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(service.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdProduct = service.createUser(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdProduct.getId()).toUri();
+        return ResponseEntity.created(location).body(createdProduct);
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable Long id,
+            @RequestBody User user) {
+        return ResponseEntity.ok(service.updateUser(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
+        service.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
